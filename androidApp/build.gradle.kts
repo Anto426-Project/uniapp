a4957cb1504c2c87568adaefcb00869f55b35f1c
Anto426 <amanocchio426@gmail.com>
2026-09-08 21:53:35 +0200

    fix: update version code and name to 2.0.3, adjust minimum supported version in update config

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.anto426.uniapp"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 202
-        versionName = "2.0.2"
+        versionCode = 203
+        versionName = "2.0.3"
         ndk {
             abiFilters += "arm64-v8a"
         }
